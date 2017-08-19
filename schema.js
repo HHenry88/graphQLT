eb40ba2620f8e7c25196f33b16c674af3979b900
Hhenry88 <henryhan88@outlook.com>
2017-08-19 11:58:24 +0800

    PersonType schema created

--- schema.js
+++ schema.js
@@ -7,6 +7,25 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
+//Person Type
+const PersonType = new GraphQLObjectType({
+    name: "Person",
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt},
+    })
+});
+
+// root Query
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    person: {
+        type: PersonType
+    }
+});
+
 module.exports = new GraphQLSchema({
-    
+
 });
