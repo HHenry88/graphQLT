e2d337c850218e6dff6717549a585f60ffe13cdc
Hhenry88 <henryhan88@outlook.com>
2017-08-19 11:13:55 +0800

    Express server for graphql

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require('express');
+var graphqlHTTP = require('express-graphql');
 var { graphql, buildSchema } = require('graphql');
 
 var schema = buildSchema(`
@@ -5,3 +7,19 @@ var schema = buildSchema(`
     hello: String
   }
 `);
+
+var root = {
+  hello: () => {
+    return 'Hello';
+  }
+};
+
+var app = express();
+app.use(`/graphql`, graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql:true
+}));
+
+app.listen(5000);
+console.log(`Running on port 5000`);
